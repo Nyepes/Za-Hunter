2692fe9e034f93947eb284dafa1d057d423bbc45
Nicolas Yepes <nicolasyepespulido@gmail.com>
2019-07-10 13:24:31 -0500

    Finished Location Details

--- Za' Hunter/LocationDetailsViewController.swift
+++ Za' Hunter/LocationDetailsViewController.swift
@@ -11,23 +11,21 @@ import MapKit
 
 class LocationDetailsViewController: UIViewController {
     
+    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var addressLabel: UILabel!
+    @IBOutlet weak var phoneLabel: UILabel!
     var selectedMapItem = MKMapItem()
     
     override func viewDidLoad() {
         super.viewDidLoad()
         print(selectedMapItem.name!)
-        // Do any additional setup after loading the view.
+        nameLabel.text = selectedMapItem.placemark.name
+        var address = selectedMapItem.placemark.subThoroughfare! + " "
+        address += selectedMapItem.placemark.thoroughfare! + "\n"
+        address += selectedMapItem.placemark.locality! + ", "
+        address += selectedMapItem.placemark.administrativeArea! + " "
+        address += selectedMapItem.placemark.postalCode!
+        addressLabel.text = address
+        phoneLabel.text = selectedMapItem.phoneNumber
     }
-    
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
 }
